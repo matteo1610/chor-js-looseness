5c795f6a8fbf87eae07a45c66f4e7e46f2f6a74a
matteo1610 <cardellini.matteo@gmail.com>
2024-05-23 11:27:55 +0200

    Added the execution tab & code refactoring

--- app/lib/provider/parts/CompositionProps.js
+++ app/lib/provider/parts/CompositionProps.js
@@ -1,37 +1,42 @@
-import { is } from 'bpmn-js/lib/util/ModelUtil';
-import tableDefinition from './implementation/TableDefinition';
+import { addTableDefinition } from './helper/TableDefinitionHelper';
 
-export default function(group, element, bpmnFactory, translate) {
-  if (is(element, 'bpmn:Choreography')) {
+/**
+ * Adds composition properties for the specified BPMN element to the properties group.
+ *
+ * @param {Object} group - The properties panel group.
+ * @param {djs.model.Base} element - The BPMN element.
+ * @param {Object} bpmnFactory - Factory to create new BPMN elements.
+ * @param {Function} translate - Function to translate labels and descriptions.
+ */
+export default function addCompositionProps(group, element, bpmnFactory, translate) {
+  const messageOptions = {
+    id: 'messageList',
+    description: 'Message List',
+    labels: 'Message',
+    addLabel: 'Add Message',
+    businessObjectProperty: 'messageItems',
+    type: 'value:MessageValue'
+  };
 
-    const options1 = {
-      id: 'messageList',
-      description: 'Message List',
-      labels: 'Message',
-      addLabel: 'Add Message',
-      businessObjectProperty: 'messageItems',
-      type: 'value:MessageValue'
-    };
-    group.entries.push(tableDefinition(element, bpmnFactory, translate, options1));
+  const attributeOptions = {
+    id: 'attributeList',
+    description: 'Attribute List',
+    labels: 'Attribute',
+    addLabel: 'Add Attribute',
+    businessObjectProperty: 'attributeItems',
+    type: 'value:AttributeValue'
+  };
 
-    const options2 = {
-      id: 'attributeList',
-      description: 'Attribute List',
-      labels: 'Attribute',
-      addLabel: 'Add Attribute',
-      businessObjectProperty: 'attributeItems',
-      type: 'value:AttributeValue'
-    };
-    group.entries.push(tableDefinition(element, bpmnFactory, translate, options2));
+  const participantOptions = {
+    id: 'participantList',
+    description: 'Participant List',
+    labels: 'Participant',
+    addLabel: 'Add Participant',
+    businessObjectProperty: 'participantItems',
+    type: 'value:ParticipantValue'
+  };
 
-    const options3 = {
-      id: 'participantList',
-      description: 'Participant List',
-      labels: 'Participant',
-      addLabel: 'Add Participant',
-      businessObjectProperty: 'participantItems',
-      type: 'value:ParticipantValue'
-    };
-    group.entries.push(tableDefinition(element, bpmnFactory, translate, options3));
-  }
+  addTableDefinition(group, element, bpmnFactory, translate, 'bpmn:Choreography', messageOptions);
+  addTableDefinition(group, element, bpmnFactory, translate, 'bpmn:Choreography', attributeOptions);
+  addTableDefinition(group, element, bpmnFactory, translate, 'bpmn:Choreography', participantOptions);
 }
